db17ecca3594e9d63344a3a1f9afdffb4c46f8fc
Annie Tang <annietang@berkeley.edu>
2018-06-24 19:31:07 -0700

    added methods - getter and setter - to DishQuery

--- src/main/java/codeu/model/data/query/DishQuery.java
+++ src/main/java/codeu/model/data/query/DishQuery.java
@@ -15,7 +15,51 @@
 package codeu.model.data.query;
 
 import codeu.model.data.Dish;
+import java.util.UUID;
 
 public class DishQuery {
+  private HashMap<UUID, Dish> dishMap; // maps dishIDs to Dish objects
+  private HashMap<UUID, int> ratingMap; // maps dishIDs to average star ratings
+
+  public DishQuery(HashMap<UUID, Dish> dishMap, HashMap<UUID, int> ratingMap) {
+    this.dishMap = dishMap;
+    this.ratingMap = ratingMap;
+  }
+
+  /** Adds a new Dish into our dishMap*/
+  public void addDish(UUID id, Dish dish) {
+    this.dishMap.put(id, dish);
+  }
+
+  /** Adds/updates the average rating for this dish */
+  public void addRating(UUID id, int rate) {
+    int oldRating = 0;
+    if (dishExists(id)) {
+      oldRating = getRating(id);
+    }
+    return (oldRating + rate) / 2;
+  }
+
+  /** Returns ture if this dish exists in our map */
+  public boolean dishExists(UUID id) {
+    returns this.dishMap.containsKey(id);
+  }
+
+  /** Returns DishMap */
+  public HashMap<UUID, Dish> getDishMap {
+    return this.dishMap;
+  }
+
+  /** Returns ratingMap */
+  public HashMap<UUID, int> getRatingMap {
+    return this.ratingMap;
+  }
+
+  /** Get rating of a particular dish.
+    * Only called under the conditions that ID exists in our dishMap */
+  public int getRating(UUID id) {
+    int rating = this.ratingMap.get(id);
+    return rating;
+  }
 
 }
