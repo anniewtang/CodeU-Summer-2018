40a9e68f6ce3b467533084366a402b8581398ae9
Chibuikem Ohanele <chibuikemohanele@Chibuikems-MacBook-Pro.local>
2018-07-02 19:05:57 -0700

    Add user funtionality with regards to about me section for the profile page

--- src/main/java/codeu/controller/ProfileServlet.java
+++ src/main/java/codeu/controller/ProfileServlet.java
@@ -15,6 +15,7 @@ import codeu.model.data.User;
 import codeu.model.store.basic.UserStore;
 
 public class ProfileServlet extends HttpServlet {
+  private User user;
 
   /**
    * Set up state for handling registration-related requests. This method is only called when
@@ -29,12 +30,28 @@ public class ProfileServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
-    request.getRequestDispatcher("/WEB-INF/view/profile.jsp").forward(request, response);
+     // Checks to see if the user is logged into his or her page
+     if (request.getSession().getAttribute("user") != null) {
+      request.getRequestDispatcher("/WEB-INF/view/profile.jsp").forward(request, response);
+      return;
+    }
+    else {
+      request.getRequestDispatcher("/WEB-INF/view/login.jsp").forward(request, response);
+      return;
+    }
   }
 
   @Override
   public void doPost(HttpServletRequest request, HttpServletResponse response)
       throws IOException, ServletException {
+        String username = (String) request.getSession().getAttribute("user");
+        user = UserStore.getInstance().getUser(username);
 
-  }
+        if (username != null) {
+            String aboutMe = request.getParameter("About Me");
+            user.setAboutMe(aboutMe);
+            UserStore.getInstance().updateUser(user);
+            response.sendRedirect("/profile");
+            }
+    }
 }
