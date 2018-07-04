c47b906d274d7da9df312d639f034e608cb75817
Chibuikem Ohanele <chibuikemohanele@Chibuikems-MacBook-Pro.local>
2018-07-04 14:59:06 -0700

    Add about me to persisten data store

--- src/main/java/codeu/model/store/persistence/PersistentDataStore.java
+++ src/main/java/codeu/model/store/persistence/PersistentDataStore.java
@@ -217,6 +217,7 @@ public class PersistentDataStore {
         userEntity.setProperty("username", user.getName());
         userEntity.setProperty("password_hash", user.getPasswordHash());
         userEntity.setProperty("creation_time", user.getCreationTime().toString());
+        userEntity.setProperty("About me",user.getAboutMe().toString());
         datastore.put(userEntity);
     }
 
