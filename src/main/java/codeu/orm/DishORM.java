b16aeea2da13d1d160f5e50f2d04d3c1213a389f
Annie Tang <annietang@berkeley.edu>
2018-07-04 10:50:40 -0700

    reformatted code

--- src/main/java/codeu/orm/DishORM.java
+++ src/main/java/codeu/orm/DishORM.java
@@ -1,4 +1,4 @@
-package codeu.orm;// Copyright 2017 Google Inc.
+// Copyright 2017 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@ package codeu.orm;// Copyright 2017 Google Inc.
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
+package codeu.orm;
 
 import codeu.model.data.Dish;
 import codeu.model.store.basic.ReviewStore;
