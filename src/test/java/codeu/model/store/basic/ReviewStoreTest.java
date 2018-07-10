6b553defcaa8c476fc6243b5cf17b9afeeb0f615
Annie Tang <annietang@berkeley.edu>
2018-07-09 21:05:43 -0700

    wrote testAddReview method for ReviewStoreTest

--- src/test/java/codeu/model/store/basic/ReviewStoreTest.java
+++ src/test/java/codeu/model/store/basic/ReviewStoreTest.java
@@ -15,19 +15,105 @@
 package codeu.model.store.basic;
 
 import codeu.TestingFramework.TestFramework;
+import codeu.model.data.Constants;
+import codeu.model.data.Review;
+import codeu.model.data.Tag;
+import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.UUID;
+import java.util.*;
 
 public class ReviewStoreTest extends TestFramework {
     @Test
     public void testGetReviewsForDish() {
-
+        Assert.assertEquals(new HashSet<>(Arrays.asList(review, reviewOne)), reviewStore.getReviewsForDish(dishID));
+        Assert.assertEquals(new HashSet<>(Arrays.asList(reviewTwo)), reviewStore.getReviewsForDish(dishIDTwo));
+        Assert.assertEquals(new HashSet<>(), reviewStore.getReviewsForDish(UUID.randomUUID()));
     }
 
+    /**
+     * Things to check for addReview:
+     * 1.) Are the dish's tags updated
+     * 2.) Are the Tags are updated
+     * 3.) Is the avg rating for the dish updated?
+     * 4.) Is the avg rating in the DishStore updated?
+     * 5.) Is it in the reviewsForDish map?
+     * 6.)
+     */
     @Test
     public void testAddReview() {
+        // run
+        reviewStore.addReview(reviewThree);
 
+        // verify
+        // check Dish's tag map
+        HashSet<String> correctRestrictions = new HashSet<>();
+        correctRestrictions.addAll(restrictionsTwo);
+        correctRestrictions.addAll(restrictionsThree);
+
+        HashSet<String> correctCuisine = new HashSet<>();
+        correctCuisine.addAll(cuisineTwo);
+
+        HashSet<String> correctDish = new HashSet<>();
+        correctDish.addAll(dishThree);
+
+        HashMap<String, Set<String>> correctTags = new HashMap<>();
+        correctTags.put(Constants.RESTRICTION, correctRestrictions);
+        correctTags.put(Constants.CUISINE, correctCuisine);
+        correctTags.put(Constants.DISH, correctDish);
+
+        Assert.assertEquals(correctTags, dishStore.getTagsForDish(dishIDTwo));
+
+        // check Dish's allTags
+        HashSet<String> correctAllTags = new HashSet<>();
+        correctAllTags.addAll(allTagValuesTwo);
+        correctAllTags.addAll(restrictionsThree);
+        correctAllTags.addAll(dishThree);
+
+        Assert.assertEquals(correctAllTags, dishStore.getAllTagsForDish(dishIDTwo));
+
+        // check all affected Tags: DishTag, RestrictionsTag
+        HashSet<UUID> restrictionVeganDishes = new HashSet<>(Arrays.asList(dishID));
+        HashSet<UUID> restrictionVegetarianDishes = new HashSet<>(Arrays.asList(dishID, dishIDTwo));
+        HashSet<UUID> restrictionGlutenFreeDishes = new HashSet<>(Arrays.asList(dishID));
+        HashSet<UUID> restrictionNutFreeDishes = new HashSet<>(Arrays.asList(dishIDTwo));
+        HashSet<UUID> restrictionDairyFreeDishes = new HashSet<>(Arrays.asList(dishIDTwo));
+        HashSet<String> restrictionAllTags = new HashSet<>(Arrays.asList(Constants.VEGAN, Constants.VEGETARIAN, Constants.GLUTENFREE, Constants.NUTFREE, Constants.DAIRYFREE));
+        HashMap<String, Set<UUID>> dishesByValueTwo = new HashMap<>();
+        dishesByValueTwo.put(Constants.VEGAN, restrictionVeganDishes);
+        dishesByValueTwo.put(Constants.VEGETARIAN, restrictionVegetarianDishes);
+        dishesByValueTwo.put(Constants.GLUTENFREE, restrictionGlutenFreeDishes);
+        dishesByValueTwo.put(Constants.NUTFREE, restrictionNutFreeDishes);
+        dishesByValueTwo.put(Constants.DAIRYFREE, restrictionDairyFreeDishes);
+        Tag correctRestrictionsTag = new Tag(restrictionType, dishesByValueTwo, restrictionAllTags);
+
+
+        HashMap<String, Set<UUID>> dishesByValueThree = new HashMap<>();
+        dishesByValueThree.put(Constants.ENTREE, new HashSet<>(Arrays.asList(dishIDTwo)));
+        HashSet<String> dishesAllTags = new HashSet<>(Arrays.asList(Constants.ENTREE));
+        Tag correctDishTag = new Tag(Constants.DISH, dishesByValueThree, dishesAllTags);
+
+        Assert.assertEquals(correctRestrictionsTag, tagStore.getTagForType(Constants.RESTRICTION));
+        Assert.assertEquals(correctDishTag, tagStore.getTagForType(Constants.DISH));
+        Assert.assertEquals(cuisineTag, tagStore.getTagForType(Constants.CUISINE));
+
+        // check Dish's average rating
+        Assert.assertEquals(3, dishStore.getDish(dishIDTwo).getRating());
+
+        // check DishStore's avg rating map
+        Assert.assertEquals(3, dishStore.getAverageRating(dishIDTwo));
+
+        // check reviewsForDish map
+        Assert.assertEquals(new HashSet<>(Arrays.asList(reviewThree, reviewTwo)), reviewStore.getReviewsForDish(dishIDTwo));
     }
 
+    /**
+     * Add a review for a NEW dish.
+     * Check to see that it was appropriately added into reviewsForDish.
+     */
+    @Test
+    public void testAddReviewForNewDish() {
+        reviewThree = new Review(UUID.randomUUID(), authorThree, dishIDTwo, numStarsThree, descThree, tagsThree);
+        reviewStoreEmpty.addReview(review);
+    }
 }
